74635f9e835d756fd749e618b02a7760175d0dcc
AlexKarp0 <alex.karpovich0@gmail.com>
2022-11-24 19:01:24 +0300

    ++x

--- src/lesson3/lesson3.java
+++ src/lesson3/lesson3.java
@@ -4,7 +4,9 @@ public class lesson3 {
     public static void main(String[] args) {
         char c = 'n';
         c++;
+        int x = 0;
         System.out.println(c);
+        System.out.println(++x);
     }
 }
 
