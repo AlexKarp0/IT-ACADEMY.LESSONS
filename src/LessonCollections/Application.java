8ff2ebb7df5464eba4047e0a2b8e5c199e51bf72
AlexKarp0 <alex.karpovich0@gmail.com>
2023-01-10 15:55:05 +0300

    Lesson collections

--- src/LessonCollections/Application.java
+++ src/LessonCollections/Application.java
@@ -1,2 +1,39 @@
-package LessonCollections;public class Application {
+package LessonCollections;
+
+import java.security.KeyPair;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Iterator;
+
+public class Application {
+    public static void main(String[] args) {
+        ArrayList<HeavyBox> heavyBoxes = new ArrayList<>();
+        heavyBoxes.add(new HeavyBox(999,999,999,999));
+        HeavyBox newHB = new HeavyBox(2,2,2,2);
+        heavyBoxes.add(newHB);//1
+        heavyBoxes.add(newHB);//2
+
+        for (HeavyBox heavyBox : heavyBoxes) {
+            System.out.println(heavyBox);
+
+        }
+        HeavyBox hb =  heavyBoxes.get(0);
+        hb.weight = 1;
+        heavyBoxes.set(0,hb);
+        for (HeavyBox heavyBox : heavyBoxes) {
+            System.out.println(heavyBox);
+
+        }
+        System.out.println("after delete");
+        heavyBoxes.remove(heavyBoxes.size()-1);
+
+        for (HeavyBox heavyBox : heavyBoxes) {
+            System.out.println(heavyBox);
+        }
+        System.out.println(heavyBoxes);
+
+        System.out.println("to arrays");
+        HeavyBox[] arr = (HeavyBox[]) heavyBoxes.toArray();
+        System.out.println(Arrays.toString(arr));
+    }
 }
--- src/LessonCollections/HeavyBox.java
+++ src/LessonCollections/HeavyBox.java
@@ -1,2 +1,25 @@
-package LessonCollections;public class HeavyBox {
+package LessonCollections;
+
+public class HeavyBox {
+    int weight;
+    int width;
+    int height;
+    int depth;
+
+    public HeavyBox(int weight, int width, int height, int depth) {
+        this.weight = weight;
+        this.width = width;
+        this.height = height;
+        this.depth = depth;
+    }
+
+    @Override
+    public String toString() {
+        return "HeavyBox{" +
+                "weight=" + weight +
+                ", width=" + width +
+                ", height=" + height +
+                ", depth=" + depth +
+                '}';
+    }
 }
